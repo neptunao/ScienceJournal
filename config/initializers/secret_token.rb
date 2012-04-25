be76c21a0e736f2f51b21478e46418ccdd0f7763
Alexander Ryabets <generalhorn@gmail.com>
2012-04-25 21:30:40 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScienceJournal::Application.config.secret_token = 'e7b9c7b7af4c3a283f7284c966aaa1b498a3ff23c1a1b9e5f592ed99c4a1f30a80ca7583e8adb73dfb4c8d5148f1ce119d4b1c98967ee7fecd620c5660613331'
