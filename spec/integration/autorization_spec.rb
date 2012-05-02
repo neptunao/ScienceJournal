1e59506d3f0b17c4356eeedcbe67098c3bb238d6
Alexander Ryabets <generalhorn@gmail.com>
2012-05-03 02:27:53 +0400

    Hide sign up link on sign in

--- spec/integration/autorization_spec.rb
+++ spec/integration/autorization_spec.rb
@@ -11,11 +11,13 @@ describe 'Autorization' do
   end
   it 'change sign in link to sign out' do
     response.should_not have_selector 'a', href: '/login', content: 'Sign in'
+    response.should_not have_selector 'a', href: '/register', content: 'Sign up'
     response.should have_selector 'a', href: '/logout', content: 'Sign out'
   end
   it 'change sign out link to sign in' do
     visit '/logout'
     response.should_not have_selector 'a', href: '/logout', content: 'Sign out'
     response.should have_selector 'a', href: '/login', content: 'Sign in'
+    response.should have_selector 'a', href: '/register', content: 'Sign up'
   end
 end
