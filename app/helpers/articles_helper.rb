06770ca5c6bd1417af27dad0ccf1ebff2449077f
Alexander Ryabets <generalhorn@gmail.com>
2012-06-07 01:50:36 +0400

    add statuses to helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,6 +5,10 @@ module ArticlesHelper
       when Article::STATUS_TO_REVIEW then 'Waiting for review'
       when Article::STATUS_REVIEWED then 'Reviewed'
       when Article::STATUS_APPROVED then 'Approved by editor and prepare to be published'
+      when Article::STATUS_REJECTED then 'Rejected by editor'
+      when Article::STATUS_REJECTED_BY_CENSOR then 'Rejected by censor and waiting for editor decision'
+      when Article::STATUS_PUBLISHED then 'Published'
+      else raise 'unknown status'
     end
   end
 
